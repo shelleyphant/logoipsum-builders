09bc1609f892361dc65fe4916d0345d504bccafb
Shelley Bassett <hello@shelleybassett.com>
2024-04-20 18:35:39 +1000

    Added assets from Figma

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,29 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `/`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `/`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
-};
+    plugins: [
+        'gatsby-plugin-postcss',
+        // 'gatsby-plugin-google-gtag',
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+    ],
+}
